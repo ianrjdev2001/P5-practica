0a9cfa83a57fddc30c078e271f1431674ed8d755
ianrjdev2001 <109057188+ianrjdev2001@users.noreply.github.com>
2023-03-10 05:08:20 -0600

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,41 @@
+class Ball
+{
+   constructor()
+   {
+        this.r = 10;
+        this.reset();
+   } 
+
+   draw()
+    {
+        ellipse(this.x, this.y, this.r * 2);
+    }
+
+    move()
+    {
+        this.x += this.stepX;
+        this.y += this.stepY;
+    }
+
+    edge()
+    {
+        if(this.y - this.r <= 0 || this.y + this.r >= height)
+        {
+            this.stepY *= -1;
+        }
+
+        if(this.x - this.r <= 0 || this.x + this.r >= width)
+        {
+            this.reset();
+        }
+    }
+
+    reset()
+    {
+        this.x = int(width / 2);
+        this.y = int(height / 2);
+        this.r = 10;
+        this.stepX = random([-1, 1]);
+        this.stepY = random([-1, 1]);
+    }
+}
---
+++ paddle.js
@@ -0,0 +1,23 @@
+class Paddle
+{
+    constructor(isLeft)
+    {
+        this.w = 20;
+        this.h = 100;
+        this.y = int(height / 2);
+
+        if(isLeft) 
+        {
+            this.x = 0;
+        }
+        else
+        {
+            this.x = width - this.w;
+        }
+    }
+
+    draw()
+    {
+        
+    }
+}
